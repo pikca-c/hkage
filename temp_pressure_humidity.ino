77f0165d060e869a828c866ed9059be3fed992ae
pikca <36289506+pikca-c@users.noreply.github.com>
2023-03-25 21:57:36 +0800

    Add files via upload

---
+++ temp_pressure_humidity.ino
@@ -0,0 +1,91 @@
+#include <SHTSensor.h>
+#include <Arduino.h>
+#include <Wire.h>
+#include <BMP180I2C.h>
+#define I2C_ADDRESS 0x77
+
+
+
+SHTSensor sht;
+BMP180I2C bmp180(I2C_ADDRESS);
+
+// To use a specific sensor instead of probing the bus use this command:
+// SHTSensor sht(SHTSensor::SHT3X);
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Wire.begin();
+  Serial.begin(9600);
+  delay(1000); // let serial console settle
+
+  if (sht.init()) {
+      Serial.print("init(): success\n");
+  } else {
+      Serial.print("init(): failed\n");
+  }
+  sht.setAccuracy(SHTSensor::SHT_ACCURACY_MEDIUM); // only supported by SHT3x
+if (!bmp180.begin())
+	{
+		Serial.println("begin() failed. check your BMP180 Interface and I2C Address.");
+		while (1);
+	}
+
+	//reset sensor to default parameters.
+	bmp180.resetToDefaults();
+
+	//enable ultra high resolution mode for pressure measurements
+	bmp180.setSamplingMode(BMP180MI::MODE_UHR);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if (sht.readSample()) {
+      Serial.print("SHT:\n");
+      Serial.print("  RH: ");
+      Serial.print(sht.getHumidity(), 2);
+      Serial.print("\n");
+      Serial.print("  T:  ");
+      Serial.print(sht.getTemperature(), 2);
+      Serial.print("\n");
+  } else {
+      Serial.print("Error in readSample()\n");
+  }
+
+  delay(1000);
+if (!bmp180.measureTemperature())
+	{
+		Serial.println("could not start temperature measurement, is a measurement already running?");
+		return;
+	}
+
+	//wait for the measurement to finish. proceed as soon as hasValue() returned true. 
+	do
+	{
+		delay(100);
+	} while (!bmp180.hasValue());
+
+	Serial.print("Temperature: "); 
+	Serial.print(bmp180.getTemperature()); 
+	Serial.println(" degC");
+
+	//start a pressure measurement. pressure measurements depend on temperature measurement, you should only start a pressure 
+	//measurement immediately after a temperature measurement. 
+	if (!bmp180.measurePressure())
+	{
+		Serial.println("could not start perssure measurement, is a measurement already running?");
+		return;
+	}
+
+	//wait for the measurement to finish. proceed as soon as hasValue() returned true. 
+	do
+	{
+		delay(100);
+	} while (!bmp180.hasValue());
+
+	Serial.print("Pressure: "); 
+	Serial.print(bmp180.getPressure());
+	Serial.println(" Pa");
+
+}
